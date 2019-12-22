ad42f5ca06b3fe31268fa0971d052b5bb1e0f927
Chan <906355296@qq.com>
2019-12-22 15:59:42 +0800

    随机分配

--- .idea/Martix_def.py
+++ .idea/Martix_def.py
@@ -2,11 +2,12 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 import seaborn as sns
+#import math
 
 #设定超参数
-M = 1               #可用基站数
-K = 3              #可用基站频点
-N = 7          #申请用户数量
+M = 4              #可用基站数
+K = 2              #可用基站频点
+N = 6               #申请用户数量
 
 def Location_matrix_df(n,m,k):
     Location_matrix = np.zeros(shape=(n,m,k),dtype=int)
@@ -24,21 +25,40 @@ def Location_matrix_show(Location_matrix):
 
 def Random_Allocation_matrix_df(n,m,k,Location_matrix):
     Random_Allocation_matrix = np.zeros(shape=(n,m,k),dtype=int)
-    for i in range (n):
-        #for j in range(m):
-            #if np.all(Location_matrix[i,j,0])==1:
+    flag = 0
+    for i in range(n):
+        for l in range(m):
+            if np.all(Location_matrix[i, l, 0]) == 1:
+                for x in range(k):
+                    for j in range(n):
+                        flag = flag + Random_Allocation_matrix[j, l, x]  # 观察x号频段是否有人使用
 
-                if  np.all(Random_Allocation_matrix[i,0,:])==1:
-                    print("User %d don't have free Frequence"  % (i))
-
-                else:
-                    for l in range (k):
-                        if np.all(Random_Allocation_matrix[:,0,l])==0:
-                            Random_Allocation_matrix[i,0,l] = 1
-                            break
+                    if flag == 1:
+                        flag = 0
+                        print("对于基站%d范围内%d号用户,频段 %d 已被占用" % (l,i,x))
+                    else:
+                        flag = 0
+                        Random_Allocation_matrix[i, l, x] = 1
+                        print("基站%d范围内%d号用户,频段 %d 成功分配" % (l, i, x))
+                        print("%d号用户,随机分配成功" % ( i))
+                        break
+        if np.sum(Random_Allocation_matrix[i,:,:])==0:
+            print("%d号用户,随机分配失败" % ( i))
 
     return Random_Allocation_matrix
 
+def I_caculate(n,m,k,Allocation_matrix):
+    I_matrix = np.zeros(shape=(n,m,k),dtype=int)
+    for l in range (k):
+        for i in range (n):
+            for j in range (m):
+                I_matrix[i,j,l] = np.sum(Allocation_matrix[:,:,l]) - np.sum(Allocation_matrix[:,j,l])
+    return I_matrix
+
+def R_caculate(n,m,k,Allocation_matrix,I_matrix):
+    Allocation_matrix_float = Allocation_matrix.astype(np.float)
+    r = np.sum(np.log2(1 + Allocation_matrix_float/I_matrix))
+    return r
 
 Location_matrix = Location_matrix_df(N, M, K)
 #print(Location_matrix)
@@ -47,3 +67,8 @@ Location_matrix = Location_matrix_df(N, M, K)
 Random_Allocation_matrix = Random_Allocation_matrix_df(N, M, K,Location_matrix)
 
 print(Random_Allocation_matrix)
+Allocation_matrix = Random_Allocation_matrix
+I_matrix = I_caculate(N, M, K,Allocation_matrix)
+print(I_matrix)
+r = R_caculate(N, M, K,Allocation_matrix,I_matrix)
+print ("对于当前矩阵，总传输速率为%g"%(r))
